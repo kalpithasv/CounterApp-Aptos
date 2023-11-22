6bf6b58c6704b425d093296d7f56bc55f6026b23
Kalpitha S V <kalpithasv@gmail.com>
2023-11-23 00:00:09 +0530

    Update LeaderBoard.jsx

--- counter/src/components/LeaderBoard.jsx
+++ counter/src/components/LeaderBoard.jsx
@@ -15,16 +15,6 @@ const LeaderBoard = ({ toggleLeaderBoard,num }) => {
       </div>
       <div className="flex flex-col gap-4 pt-5 h-[calc(100vh-5rem)] overflow-y-auto scrollbar-hide">
         <LeaderBoardCard num={num} address={"0x462Ba8a537DCcb8A5eD3e0850752A101ed4B478d"}/>
-        <LeaderBoardCard num={3} address={"0x789798se89fee897A101ed4B478d..."}/>
-        <LeaderBoardCard num={3} address={"0x679238hjknckjnnvkmekmvlkemrm..."}/>
-        <LeaderBoardCard num={3} address={"0x54kj787f97d98d8989escx009d89..."}/>
-        <LeaderBoardCard num={2} address={"0x89sdvre89dcer7r8erev78rver6v..."}/>
-        <LeaderBoardCard num={2} address={"0xDC24316b9AE028F1497c275EB919..."}/>
-        <LeaderBoardCard num={2} address={"0x34dejhew98we89ew989efw4B478d..."}/>
-        <LeaderBoardCard num={1} address={"0x78efwefefe8tgt7ee901ed4B478d..."}/>
-        <LeaderBoardCard num={1} address={"0x67efer8f7r8re8fewf9864bB478d..."}/>
-        <LeaderBoardCard num={1} address={"0x09f8r9frgt8e85fn7A101ed4B47mk..."}/>
-
       </div>
     </div>
   );
